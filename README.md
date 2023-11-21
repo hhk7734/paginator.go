76da64a23ac32961666ebbcd8b722cd8da468a4f
Hyeonki Hong <hhk7734@gmail.com>
2023-11-21 22:34:08 +0900

    feat(offset): add OffsetModel

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## paginator.go
 
 ### Offset
-
-- page: page index (start from 0)
-- size: page size
--- offset.go
+++ offset.go
@@ -1,3 +1,23 @@
 package paginator
 
-type Offset struct{}
+type Offset struct {
+	// Page is the current page. Page numbering starts at 0.
+	Page int64
+	// Size is the number of items per page.
+	Size int64
+}
+
+type OffsetModel struct {
+	// Page is the current page. Page numbering starts at 0.
+	Page int64 `json:"page"`
+	// Size is the number of items per page.
+	Size int64 `json:"size"`
+	// TotalItems is the total number of items.
+	TotalItems int64 `json:"total_items"`
+	// TotalPages is the total number of pages.
+	TotalPages int64 `json:"total_pages"`
+	// First is true if the current page is the first page.
+	First bool `json:"first"`
+	// Last is true if the current page is the last page.
+	Last bool `json:"last"`
+}
